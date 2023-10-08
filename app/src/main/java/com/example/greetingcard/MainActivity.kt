f938a43fb315547aed98efafc22ec1ad4e09ff64
rlopezes <146133734+rlopezes@users.noreply.github.com>
2023-10-08 22:56:40 +0200

    Update MainActivity.kt
    Cambio nombre

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -46,6 +46,6 @@ fun Greeting(name: String, color : Color, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GreetingCardTheme {
-        Greeting("Rubén", Color(0xFFFF9900))
+        Greeting("asdfasdf", Color(0xFFFF9900))
     }
 }
